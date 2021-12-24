0804fbdcd529f7ee2a4a69d2871536857bc698a2
Tamandeep Singh <tamandeepsingh999@gmail.com>
2021-12-24 23:24:17 +0530

    Solutions

---
+++ Dynamic Programming/Bookshop.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod=1e9+7;
+
+int main()
+{
+	
+	int n,x;
+	cin>>n>>x;
+	vector<int> h(n),s(n);
+	for(int i=0;i<n;i++)
+		cin>>s[i];
+	for(int i=0;i<n;i++)
+		cin>>h[i];
+	int c=0;
+	vector<vector<int>> dp(n,vector<int> (x+1,0));
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<=x;j++)
+		{
+			c++;
+			if(j==0)
+			{
+				dp[i][j]=0;
+				continue;
+			}
+			if(i>0 and j>=s[i])
+			{
+				dp[i][j]=max(dp[i-1][j],h[i-1]+dp[i-1][j-s[i]]);
+			//	cout<<dp[i][j]<<endl;
+			}
+			else
+				if(i==0  and j>=s[0])
+					dp[0][j]=h[0];
+
+		}
+	}
+	cout<<dp[n-1][x];
+	return 0;
+}
--- Dynamic Programming/coin combinations 2.cpp
+++ Dynamic Programming/coin combinations 2.cpp
@@ -11,18 +11,22 @@ int main()
 	for(int i=0;i<n;i++)
 		cin>>a[i];
 	vector<vector<int>> dp(n,vector<int> (x+1,0));
-	
-	for(int i=0;i<n;i++)
+	dp[0][a[0]]=1;
+	for(int i=1;i<n;i++)
 	{
 		for(int j=0;j<=x;j++)
 		{
 			if(j==0 )
+			{
 				dp[i][j]=1;
-			if(i==1)
-				dp[i][a[i]]=1;
-			if(j-a[i-1]<0)
 				continue;
-			dp[i][j]=dp[i-1][j-a[i-1]]+dp[i-1][j];
+			}
+			int option1=0,option2=0;
+			if(j-a[i]>=0)
+				option1=dp[i][j-a[i]];
+			if(i==0 and j%a[i]==0)
+				option2=1;
+            dp[i][j]=option2+option1;
 			dp[i][j]%=mod;
 		}
 	}
